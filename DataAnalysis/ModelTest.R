723e30a5e049d5cf8d0e17ea6c23e20a58fcc8d4
Matthew Farr <matthewtfarr@gmail.com>
2020-01-14 16:34:09 -0500

    Add phases to model. Update figure generator.

--- DataAnalysis/ModelTest.R
+++ DataAnalysis/ModelTest.R
@@ -220,19 +220,23 @@ params2 <- list("beta0" = opt1$par[1], "beta1" = opt1$par[2],
                 "log_kappa_su" = 0, "log_tau_su" = 1, "omega_su" = rep(0, su.nodes))
 
 map2 <- list(
-             #"alpha0" = as.factor(NA), 
-             #"alpha1" = as.factor(NA),
-             #"beta0" = as.factor(NA), 
-             #"beta1" = as.factor(NA)
-             "log_kappa_sp1" = as.factor(NA),
-             "log_tau_sp1" = as.factor(NA),
-             "omega_sp1" = factor(rep(NA, sp.nodes)),
-             "log_kappa_sp2" = as.factor(NA),
-             "log_tau_sp2" = as.factor(NA),
-             "omega_sp2" = factor(rep(NA, sp.nodes)),
-             "log_kappa_su" = as.factor(NA),
-             "log_tau_su" = as.factor(NA),
-             "omega_su" = factor(rep(NA, su.nodes))
+             "alpha0" = as.factor(NA), 
+             "alpha1" = as.factor(NA),
+             "beta0" = as.factor(NA), 
+             "beta1" = as.factor(NA),
+             "delta1" =  as.factor(NA),
+             "delta2" = as.factor(NA),
+             "gamma1" = as.factor(NA),
+             "gamma2" = as.factor(NA)
+             #"log_kappa_sp1" = as.factor(NA),
+             #"log_tau_sp1" = as.factor(NA),
+             #"omega_sp1" = factor(rep(NA, sp.nodes)),
+             #"log_kappa_sp2" = as.factor(NA),
+             #"log_tau_sp2" = as.factor(NA),
+             #"omega_sp2" = factor(rep(NA, sp.nodes)),
+             #"log_kappa_su" = as.factor(NA),
+             #"log_tau_su" = as.factor(NA),
+             #"omega_su" = factor(rep(NA, su.nodes))
              )
 
 #Random effects
@@ -248,89 +252,64 @@ obj2$env$tracepar <- TRUE
 opt2 <- nlminb(obj2$par, obj2$fn, obj2$gr)
 out2 <- sdreport(obj2)
 
-#PHASE 3: ???
+#PHASE 3: Refit fixed parameters (not related to random effects)
 
 #Parameters to estimate
-par.omg <- out2$par.random[1:nodes]
-par.eps <- out2$par.random[(nodes+1):(nodes + nodes*t_n)]
-dim(par.eps) <- c(nodes, t_n)
-params3 <- list("beta0" = opt1$par[1:t_n], "beta1" = 0, "alpha0" = 5, "alpha1" = 0,
-                "log_kappa" = out2$par.fixed[1], "log_tau_O" = out2$par.fixed[2], "log_tau_E" = out2$par.fixed[3],
-                "omega" = par.omg, "epsilon" = par.eps)
+par.omg_sp1 <- out2$par.random[1:sp.nodes]
+par.omg_sp2 <- out2$par.random[(sp.nodes+1):(sp.nodes*2)]
+par.omg_su <- out2$par.random[(sp.nodes*2+1):(sp.nodes*2+su.nodes)]
+params3 <- list("beta0" = opt1$par[1], "beta1" = opt1$par[2],
+                "delta1" = opt1$par[3], "delta2" = opt1$par[4],
+                "gamma1" = opt1$par[5], "gamma2" = opt1$par[6],
+                "alpha0" = opt1$par[7], "alpha1" = opt1$par[8],
+                "log_kappa_sp1" = out2$par.fixed[1], "log_tau_sp1" = out2$par.fixed[4], "omega_sp1" = par.omg_sp1,
+                "log_kappa_sp2" = out2$par.fixed[2], "log_tau_sp2" = out2$par.fixed[5], "omega_sp2" = par.omg_sp2,
+                "log_kappa_su" = out2$par.fixed[3], "log_tau_su" = out2$par.fixed[6], "omega_su" = par.omg_su)
 
 map3 <- list(
-  "alpha0" = as.factor(NA), 
-  #"alpha1" = as.factor(NA),
-  "beta0" = factor(rep(NA, t_n)), 
-  "beta1" = as.factor(NA),
-  "log_kappa" = as.factor(NA), 
-  "log_tau_O" = as.factor(NA), 
-  "log_tau_E" = as.factor(NA),
-  "omega" = factor(rep(NA, nodes)), 
-  "epsilon" = map.eps
+  # "alpha0" = as.factor(NA), 
+  # "alpha1" = as.factor(NA),
+  # "beta0" = as.factor(NA), 
+  # "beta1" = as.factor(NA),
+  # "delta1" =  as.factor(NA),
+  # "delta2" = as.factor(NA),
+  # "gamma1" = as.factor(NA),
+  # "gamma2" = as.factor(NA)
+  "log_kappa_sp1" = as.factor(NA),
+  "log_tau_sp1" = as.factor(NA),
+  "omega_sp1" = factor(rep(NA, sp.nodes)),
+  "log_kappa_sp2" = as.factor(NA),
+  "log_tau_sp2" = as.factor(NA),
+  "omega_sp2" = factor(rep(NA, sp.nodes)),
+  "log_kappa_su" = as.factor(NA),
+  "log_tau_su" = as.factor(NA),
+  "omega_su" = factor(rep(NA, su.nodes))
 )
 
-#Random effects
-random <- c("omega", "epsilon")
-
 #Make AD objective function
-obj3 <- MakeADFun(data = data, parameters = params3, random = random, map = map3, DLL="ModelTest")
+obj3 <- MakeADFun(data = data2, parameters = params3, random = random, map = map3, DLL="IM_random_separate")
 
 #Trace parameters
 obj3$env$tracepar <- TRUE
 
 #Minimize objective function
 opt3 <- nlminb(obj3$par, obj3$fn, obj3$gr)
-
-#PHASE 4: ???
-
-#Parameters to estimate
-params4 <- list("beta0" = opt1$par[1:t_n], "beta1" = 0, "alpha0" = opt3$par[1], "alpha1" = opt3$par[2],
-                "log_kappa" = out2$par.fixed[1], "log_tau_O" = out2$par.fixed[2], "log_tau_E" = out2$par.fixed[3],
-                "omega" = par.omg, "epsilon" = par.eps)
-
-map4 <- list(
-  "alpha0" = as.factor(NA), 
-  "alpha1" = as.factor(NA),
-  "beta0" = factor(rep(NA, t_n)), 
-  #"beta1" = as.factor(NA),
-  "log_kappa" = as.factor(NA), 
-  "log_tau_O" = as.factor(NA), 
-  "log_tau_E" = as.factor(NA),
-  "omega" = factor(rep(NA, nodes)), 
-  "epsilon" = map.eps
-)
-
-#Random effects
-random <- c("omega", "epsilon")
-
-#Make AD objective function
-obj4 <- MakeADFun(data = data, parameters = params4, random = random, map = map4, DLL="ModelTest")
-
-#Trace parameters
-obj4$env$tracepar <- TRUE
-
-#Minimize objective function
-opt4 <- nlminb(obj4$par, obj4$fn, obj4$gr)
+out3 <- sdreport(obj3)
 
 #-Parameter values-#
 
-beta0 <- opt1$par[1:t_n]
-beta1 <- opt4$par[1]
-alpha0 <- opt3$par[1]
-alpha1 <- opt3$par[2]
-tauO <- exp(out2$par.fixed[2])
-tauE <- exp(out2$par.fixed[3])
-omg <- out2$par.random[1:nodes]/tauO
-eps <- out2$par.random[(nodes+1):(nodes + nodes*t_n)]/tauE
-dim(eps) <- c(nodes, t_n)
-
-beta0 <- opt1$par[1]
-beta1 <- opt1$par[2]
-delta1 <- opt1$par[3]
-delta2 <- opt1$par[4]
-gamma1 <- opt1$par[5]
-gamma2 <- opt1$par[6]
+beta0 <- out3$par.fixed[1]
+beta1 <- out3$par.fixed[2]
+delta1 <- out3$par.fixed[3]
+delta2 <- out3$par.fixed[4]
+gamma1 <- out3$par.fixed[5]
+gamma2 <- out3$par.fixed[6]
+tau_sp1 <- exp(out2$par.fixed[4])
+tau_sp2 <- exp(out2$par.fixed[5])
+tau_su <- exp(out2$par.fixed[6])
+omg_sp1 <- par.omg_sp1/tau_sp1
+omg_sp2 <- par.omg_sp2/tau_sp2
+omg_su <- par.omg_su/tau_su
 
 intercept <- NULL
 intercept[1] <- beta0 + delta1 + delta2
@@ -379,11 +358,23 @@ su.NDVI[,3] <- 0.0001*raster::values(raster::overlay(NDVIsummer[[7]], NDVIsummer
 sp.NDVI <- (sp.NDVI - mean(sp.NDVI))/sd(sp.NDVI)
 su.NDVI <- (su.NDVI - mean(su.NDVI))/sd(su.NDVI)
 
+
+sp.Pred <- cbind(exp(intercept[1] + beta1 * sp.NDVI[,1] + sp.GridA %*% omg_sp1), 
+              exp(intercept[2] + beta1 * sp.NDVI[,2] + sp.GridA %*% omg_sp2),
+              exp(intercept[3] + beta1 * sp.NDVI[,3] + sp.GridA %*% omg_sp1), 
+              exp(intercept[4] + beta1 * sp.NDVI[,4] + sp.GridA %*% omg_sp2),
+              exp(intercept[5] + beta1 * sp.NDVI[,5] + sp.GridA %*% omg_sp1), 
+              exp(intercept[6] + beta1 * sp.NDVI[,6] + sp.GridA %*% omg_sp2))
+
+su.Pred <- cbind(exp(intercept[7] + beta1 * su.NDVI[,1] + su.GridA %*% omg_su),
+              exp(intercept[8] + beta1 * su.NDVI[,2] + su.GridA %*% omg_su),
+              exp(intercept[9] + beta1 * su.NDVI[,3] + su.GridA %*% omg_su))
+
+
 tp <- cbind(c(1,2,1,2,1,2,3,3,3), c(1,1,2,2,3,3,1,2,3))
 
 for(t in 1:6){
-  Pred <- exp(intercept[t] + beta1 * sp.NDVI[,t])
-  raster::values(sp.intensity) <- as.vector(Pred)
+  raster::values(sp.intensity) <- as.vector(sp.Pred[,t])
   sp.intensity <- as(sp.intensity, "SpatialPixelsDataFrame")
   sp.intensity <- st_as_sf(sp.intensity)
   sp.intensity <- sp.intensity[domain_sp,]
@@ -397,8 +388,7 @@ for(t in 1:6){
 }
 
 for(t in 1:3){
-  Pred <- exp(intercept[t+6] + beta1 * su.NDVI[,t])
-  raster::values(su.intensity) <- as.vector(Pred)
+  raster::values(su.intensity) <- as.vector(su.Pred[,t])
   su.intensity <- as(su.intensity, "SpatialPixelsDataFrame")
   su.intensity <- st_as_sf(su.intensity)
   su.intensity <- su.intensity[domain_sm,]
@@ -414,6 +404,8 @@ for(t in 1:3){
 df <- df[-1,]
 
 df1 <- st_as_sf(df, coords = c("x", "y"), crs = prj)
+
+pal3 <- colorRampPalette(RColorBrewer::brewer.pal(11, 'Spectral'))(100)
 
 Figure1 <- ggplot(data = df) + 
   #geom_sf(data = Data, size = 0.25) + 
@@ -431,4 +423,43 @@ Figure1 <- ggplot(data = df) +
 
 Figure1
 
+#-Spatial random effect-#
+
+re.raster <- raster::raster(domain_sp, resolution = c(1, 1))
+
+for(t in 1:3){
+  raster::values(su.intensity) <- as.vector(su.Pred[,t])
+  su.intensity <- as(su.intensity, "SpatialPixelsDataFrame")
+  su.intensity <- st_as_sf(su.intensity)
+  su.intensity <- su.intensity[domain_sm,]
+  su.intensity <- as.data.frame(as(su.intensity, "Spatial"))
+  su.intensity$period <- tp[t+6,1]
+  su.intensity$year <- tp[t+6,2]
+  colnames(su.intensity) <- c("value", "x", "y", "period", "year")
+  df <- rbind(df, su.intensity)
+  rm(su.intensity)
+  su.intensity <- raster::raster(domain_sm, resolution = c(1, 1))
+}
+
+df <- df[-1,]
+
+df1 <- st_as_sf(df, coords = c("x", "y"), crs = prj)
+
+pal3 <- colorRampPalette(RColorBrewer::brewer.pal(11, 'Spectral'))(100)
+
+Figure1 <- ggplot(data = df) + 
+  #geom_sf(data = Data, size = 0.25) + 
+  geom_raster(data = df, aes(x = x, y = y, fill = value), alpha = 0.75) +
+  #geom_sf(data = domain, alpha = 0, linetype = "dashed", lwd = 1) +
+  # scale_fill_gradient2(low = "#EDEDED", mid = "#FFCCCC", high = "#660000",
+  #                      labels = c(0,50,312,625), breaks = c(0,50,312,625),
+  #                      limits=c(0, 625), oob = scales::squish) +
+  scale_fill_gradientn(colors = pal3) +
+  facet_wrap(~period + year) +
+  #coord_sf(crs = st_crs("+proj=utm +zone=14 +ellps=GRS80 +units=km +no_defs")) + 
+  theme_bw() +
+  theme(panel.background = element_blank(),
+        panel.grid = element_blank())
+
+Figure1
 
